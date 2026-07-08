ae0a2fe07b164aaa1538b73f7a5c02fc04eafbdb
mudgal <mudgal@Code>
2026-07-08 22:33:38 +0530

    Add Angular models and environment

--- Frontend/todo-ui/src/app/app.config.ts
+++ Frontend/todo-ui/src/app/app.config.ts
@@ -1,11 +1,11 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideHttpClient } from '@angular/common/http';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({
-      eventCoalescing: true
-    }),
-    provideHttpClient()
+    provideBrowserGlobalErrorListeners(),
+    provideRouter(routes)
   ]
 };
